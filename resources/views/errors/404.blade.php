d5a958a72718c04bd44838d532cb87a23802d65d
macbookpro <dev.mohamedgouda@gmail.com>
2023-12-16 10:58:17 +0200

    fix bugs in routes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
 	<img src="{{ asset('images/404.png') }}" alt="404" width="200px" height="200px">
 	<h1>Oops, looks like we could not find the page you requested</h1>
 	<br>
-	<a href="{{ route('home') }}" class="btn btn-info btn-wide">
+	<a href="{{ route('endUser.home') }}" class="btn btn-info btn-wide">
 		<i class="fa fa-arrow-circle-left"></i> Back to home page</a>
 </div>
 @endsection
