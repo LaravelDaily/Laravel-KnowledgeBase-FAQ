8afde1e14889a10b57913c53e3e80e0eb1b268bd
Abderazek Emad <a.razek@gmail.com>
2023-12-17 02:43:50 +0200

    create categories tabs for faqs

--- resources/views/endUser/pages/faqs.blade.php
+++ resources/views/endUser/pages/faqs.blade.php
@@ -24,19 +24,39 @@
     {{-- Start Faqs Section  --}}
     <section class="faqs">
         <div class="container">
-            <div class="row align-items-center g-3">
-                <div class="col-lg-6 mb-3 mb-lg-0 order-1">
+            <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-carigory-1-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-carigory-1" type="button" role="tab" aria-controls="pills-carigory-1"
+                        aria-selected="true">carigory-1</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-carigory-2-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-carigory-2" type="button" role="tab" aria-controls="pills-carigory-2"
+                        aria-selected="false">carigory-2</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-carigory-3-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-carigory-3" type="button" role="tab" aria-controls="pills-carigory-3"
+                        aria-selected="false">carigory-3</button>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-carigory-1" role="tabpanel"
+                    aria-labelledby="pills-carigory-1-tab" tabindex="0">
                     <div id="accordionExample" class="accordion h-100">
-                        @foreach($faqs as $faq)
+                        @foreach ($faqs as $faq)
                             <div class="accordion-item">
                                 <h3 id="heading{{ $loop->iteration }}" class="accordion-header">
                                     <button type="button" class="accordion-button" data-bs-toggle="collapse"
-                                            data-bs-target="#collapse{{ $loop->iteration }}" aria-expanded="true" aria-controls="collapse{{ $loop->iteration }}">
-                                       {{ $faq->question }}
+                                        data-bs-target="#collapse{{ $loop->iteration }}" aria-expanded="true"
+                                        aria-controls="collapse{{ $loop->iteration }}">
+                                        {{ $faq->question }}
                                     </button>
                                 </h3>
-                                <div id="collapse{{ $loop->iteration }}" class="accordion-collapse collapse {{ $loop->first ? 'show' : '' }}" aria-labelledby="heading{{ $loop->iteration }}"
-                                     data-bs-parent="#accordionExample">
+                                <div id="collapse{{ $loop->iteration }}"
+                                    class="accordion-collapse collapse {{ $loop->first ? 'show' : '' }}"
+                                    aria-labelledby="heading{{ $loop->iteration }}" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <p>
                                             {{ $faq->answer }}
@@ -45,14 +65,56 @@
                                 </div>
                             </div>
                         @endforeach
-
                     </div>
                 </div>
-                <div class="col-lg-6 mb-3 mb-lg-0 order-lg-2">
-                    <div class="text-center">
-                        <img class="wow slideInUp" height="100%" width="100%"
-                            src="{{ asset('assetsEndUser/content/assets/img/faqs/faqs-img.svg') }}" alt=""
-                            title="">
+                <div class="tab-pane fade" id="pills-carigory-2" role="tabpanel" aria-labelledby="pills-carigory-2-tab"
+                    tabindex="0">
+                    <div id="accordionExample" class="accordion h-100">
+                        @foreach ($faqs as $faq)
+                            <div class="accordion-item">
+                                <h3 id="heading{{ $loop->iteration }}" class="accordion-header">
+                                    <button type="button" class="accordion-button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapse{{ $loop->iteration }}" aria-expanded="true"
+                                        aria-controls="collapse{{ $loop->iteration }}">
+                                        {{ $faq->question }}
+                                    </button>
+                                </h3>
+                                <div id="collapse{{ $loop->iteration }}"
+                                    class="accordion-collapse collapse {{ $loop->first ? 'show' : '' }}"
+                                    aria-labelledby="heading{{ $loop->iteration }}" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <p>
+                                            {{ $faq->answer }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="pills-carigory-3" role="tabpanel" aria-labelledby="pills-carigory-3-tab"
+                    tabindex="0">
+                    <div id="accordionExample" class="accordion h-100">
+                        @foreach ($faqs as $faq)
+                            <div class="accordion-item">
+                                <h3 id="heading{{ $loop->iteration }}" class="accordion-header">
+                                    <button type="button" class="accordion-button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapse{{ $loop->iteration }}" aria-expanded="true"
+                                        aria-controls="collapse{{ $loop->iteration }}">
+                                        {{ $faq->question }}
+                                    </button>
+                                </h3>
+                                <div id="collapse{{ $loop->iteration }}"
+                                    class="accordion-collapse collapse {{ $loop->first ? 'show' : '' }}"
+                                    aria-labelledby="heading{{ $loop->iteration }}" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <p>
+                                            {{ $faq->answer }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
